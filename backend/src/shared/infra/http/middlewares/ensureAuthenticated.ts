516dac45f9afdad4ecd97d45cf99d9f74b06606f
lorenzowind <lorenzomart01@gmail.com>
2020-08-25 19:03:02 -0400

    chore: authentication middleware and providers config

---
+++ backend/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import AppError from '@shared/errors/AppError';
+
+import authConfig from '@config/auth';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+  const positionHeader = request.headers.user_position;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  if (positionHeader !== 'admin') {
+    throw new AppError('Only administrators can make module operations', 400);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch (err) {
+    throw new AppError('Invalid JWT token', 400);
+  }
+}
