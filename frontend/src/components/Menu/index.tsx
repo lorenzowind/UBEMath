cf0d36ab5f2d57f6b7472238cfd8d45720ca9fac
lorenzowind <lorenzomart01@gmail.com>
2020-09-30 20:24:09 -0400

    feat: interface opened menu component

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 
-import { Container, Content } from './styles';
+import {
+  ContainerClosed,
+  ContainerOpened,
+  ContentClosed,
+  ContentOpened,
+  OptionsContainer,
+  Background,
+} from './styles';
 
 const Menu: React.FC = () => {
+  const [menuIsOpened, setMenuIsOpened] = useState(false);
+
+  const handleClickMenu = useCallback(() => {
+    setMenuIsOpened(!menuIsOpened);
+  }, [menuIsOpened]);
+
   return (
-    <Container>
-      <Content>
-        <button type="button">
-          <FiMenu />
-        </button>
-      </Content>
-    </Container>
+    <>
+      {menuIsOpened ? (
+        <ContainerOpened>
+          <Background />
+
+          <ContentOpened>
+            <strong>MENU</strong>
+
+            <button type="button">
+              <FiMenu onClick={handleClickMenu} />
+            </button>
+          </ContentOpened>
+
+          <OptionsContainer>
+            <button type="button">
+              <strong>CAMPANHA</strong>
+            </button>
+            <button type="button">
+              <strong>TREINO</strong>
+            </button>
+            <button type="button">
+              <strong>MÓDULOS</strong>
+            </button>
+            <button type="button">
+              <strong>CONQUISTAS</strong>
+            </button>
+            <button type="button">
+              <strong>CONFIGURAÇÕES</strong>
+            </button>
+          </OptionsContainer>
+
+          <footer>
+            <strong>UBEMath © 2020</strong>
+          </footer>
+        </ContainerOpened>
+      ) : (
+        <ContainerClosed>
+          <ContentClosed>
+            <button type="button">
+              <FiMenu onClick={handleClickMenu} />
+            </button>
+          </ContentClosed>
+        </ContainerClosed>
+      )}
+    </>
   );
 };
 
