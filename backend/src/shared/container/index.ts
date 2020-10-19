c7db9dbe521b5f3d8cc4906dfb20debd1a3ba454
lorenzowind <lorenzomart01@gmail.com>
2020-10-19 01:03:40 -0400

    feat: routes and injections addition

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -9,6 +9,24 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
+import ILevelsRepository from '@modules/levels/repositories/ILevelsRepository';
+import LevelsRepository from '@modules/levels/infra/typeorm/repositories/LevelsRepository';
+
+import IModulesRepository from '@modules/modules/repositories/IModulesRepository';
+import ModulesRepository from '@modules/modules/infra/typeorm/repositories/ModulesRepository';
+
+import ISubModulesRepository from '@modules/sub-modules/repositories/ISubModulesRepository';
+import SubModulesRepository from '@modules/sub-modules/infra/typeorm/repositories/SubModulesRepository';
+
+import IQuestionsRepository from '@modules/questions/repositories/IQuestionsRepository';
+import QuestionsRepository from '@modules/questions/infra/typeorm/repositories/QuestionsRepository';
+
+import IAlternativesRepository from '@modules/alternatives/repositories/IAlternativesRepository';
+import AlternativesRepository from '@modules/alternatives/infra/typeorm/repositories/AlternativesRepository';
+
+import IConquestsRepository from '@modules/conquests/repositories/IConquestsRepository';
+import ConquestsRepository from '@modules/conquests/infra/typeorm/repositories/ConquestsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
@@ -17,4 +35,34 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+
+container.registerSingleton<ILevelsRepository>(
+  'LevelsRepository',
+  LevelsRepository,
+);
+
+container.registerSingleton<IModulesRepository>(
+  'ModulesRepository',
+  ModulesRepository,
+);
+
+container.registerSingleton<ISubModulesRepository>(
+  'SubModulesRepository',
+  SubModulesRepository,
+);
+
+container.registerSingleton<IQuestionsRepository>(
+  'QuestionsRepository',
+  QuestionsRepository,
+);
+
+container.registerSingleton<IAlternativesRepository>(
+  'AlternativesRepository',
+  AlternativesRepository,
+);
+
+container.registerSingleton<IConquestsRepository>(
+  'ConquestsRepository',
+  ConquestsRepository,
 );
--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -4,10 +4,26 @@ import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
+import levelsRouter from '@modules/levels/infra/http/routes/levels.routes';
+import modulesRouter from '@modules/modules/infra/http/routes/modules.routes';
+import subModulesRouter from '@modules/sub-modules/infra/http/routes/sub-modules.routes';
+import questionsRouter from '@modules/questions/infra/http/routes/questions.routes';
+import alternativesRouter from '@modules/alternatives/infra/http/routes/alternatives.routes';
+
+import conquestsRouter from '@modules/conquests/infra/http/routes/conquests.routes';
+
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+
+routes.use('/levels', levelsRouter);
+routes.use('/modules', modulesRouter);
+routes.use('/sub-modules', subModulesRouter);
+routes.use('/questions', questionsRouter);
+routes.use('/alternatives', alternativesRouter);
+
+routes.use('/conquests', conquestsRouter);
 
 export default routes;
